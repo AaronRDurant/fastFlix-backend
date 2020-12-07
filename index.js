3626a835d26066a41565d3e22d2ed8828d4b1141
Aaron Durant <aaron@aarondurant.com>
2020-12-06 19:03:12 -0500

    Added endpoints with basic validation
    Implemented routing for all endpoints listed in documentation with simple text-based responses for testing in Postman.

--- index.js
+++ index.js
@@ -51,16 +51,58 @@ let topTenMovies = [
 ];
 
 app.get('/', (req, res) => {
-	res.send('Welcome to fastFlix!');
+  res.send('Welcome to fastFlix!');
 });
 
+// Get a list of data about all movies
 app.get('/movies', (req, res) => {
-	res.json(topTenMovies);
+  res.send('Successful GET request returning data on all movies');
+ });
+
+// Get data about a movie by title
+app.get('/movies/:title', (req, res) => {
+  res.send('Successful GET request returning data on movie title: ' + req.params.title);
+ });
+
+// Get data about a genre by title
+app.get('/movies/genres/:genre', (req, res) => {
+  res.send('Successful GET request returning data on genre: ' + req.params.genre);
+ });
+
+// Get data about a director by name
+app.get('/movies/directors/:name', (req, res) => {
+  res.send('Successful GET request returning data on director: ' + req.params.name);
+});
+
+// Register new user
+app.post('/users', (req, res) => {
+  res.send('Successful POST request registering new user');
+});
+
+// Update user information
+app.put('/users/:username', (req, res) => {
+  res.send('Successful PUT request updating information for user: ' + req.params.username);
+});
+
+// Post new movie to  list of user's favorite movies
+app.post('/users/:username/movies/:movieID', (req, res) => {
+  res.send('Successful POST request adding movie with ID: ' + req.params.movieID + ' to favorite movie list of User: ' + req.params.username);
+});
+
+// Delete a movie from list of user's favorite movies
+app.delete('/users/:username/movies/:movieID', (req, res) => {
+  res.send('Successful DELETE request removing movie with ID: ' + req.params.movieID + ' from favorite movie list of User: ' + req.params.username);
+});
+
+// Delete a user
+app.delete('/users/:username', (req, res) => {
+  res.send('Successful DELETE request removing User: ' + req.params.username + ' from database');
 });
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send('Something went wrong.');
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
-app.listen(8080, () => console.log('Your app is listening on port 8080.'));
+app.listen(8080, () =>
+  console.log('Your app is listening on port 8080.'));
