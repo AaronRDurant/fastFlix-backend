636a359bc8380d8848309105b5ea8c63231857aa
Aaron Durant <aaron@aarondurant.com>
2021-01-25 20:58:59 -0500

    Enable CORS for local React app/all domains

--- index.js
+++ index.js
@@ -24,21 +24,22 @@ const passport = require('passport');
 require('./passport');
 
 const cors = require('cors');
+app.use(cors());
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
 const { check, validationResult } = require('express-validator');
 
-app.use(cors({
-	origin: (origin, callback) => {
-		if(!origin) return callback(null, true);
-		if(allowedOrigins.indexOf(origin) === -1){
-			let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-			return callback(new Error(message), false);
-		}
-		return callback (null, true);
-	}
-}));
+// app.use(cors({
+// 	origin: (origin, callback) => {
+// 		if(!origin) return callback(null, true);
+// 		if(allowedOrigins.indexOf(origin) === -1){
+// 			let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+// 			return callback(new Error(message), false);
+// 		}
+// 		return callback (null, true);
+// 	}
+// }));
 
 // Get home page
 app.get('/', (req, res) => {
