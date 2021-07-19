06aa0c301d9a62f62c0fe589dd6c98a02023fbd3
Aaron Durant <aaron@aarondurant.com>
2021-07-19 15:43:47 -0400

    Simplify CORS config to allow all headers
    To help with development of fastFlix-Angular-client.

--- index.js
+++ index.js
@@ -21,22 +21,15 @@ app.use(bodyParser.json());
 const passport = require('passport');
 require('./passport');
 
-const cors = require('cors');
-
-let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234','https://fastflixdb.herokuapp.com'];
-
 const { check, validationResult } = require('express-validator');
 
-app.use(cors({
-	origin: (origin, callback) => {
-		if(!origin) return callback(null, true);
-		if(allowedOrigins.indexOf(origin) === -1){
-			let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-			return callback(new Error(message), false);
-		}
-		return callback (null, true);
-	}
-}));
+const cors = require('cors');
+
+app.use(
+	cors({
+		allowedHeaders: '*',
+	})
+);
 
 let auth = require('./auth')(app);
 
